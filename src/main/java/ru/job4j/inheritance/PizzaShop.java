4a9f64018b79bc156229d9d0b53b6bcdef85394f
mitredo <dimon4ikac89@gmail.com>
2026-01-19 17:22:48 +0300

    6. Вызов переопределенного метода родителя - super.method ... [#504871 #557210]

--- src/main/java/ru/job4j/inheritance/PizzaShop.java
+++ src/main/java/ru/job4j/inheritance/PizzaShop.java
@@ -5,6 +5,7 @@ public class PizzaShop {
         Pizza pizza = new Pizza();
         PizzaExtraCheese pizzaExtraCheese = new PizzaExtraCheese();
         PizzaExtraCheeseExtraTomato pizzaExtraCheeseExtraTomato = new PizzaExtraCheeseExtraTomato();
+
         System.out.println(pizza.name());
         System.out.println(pizzaExtraCheese.name());
         System.out.println(pizzaExtraCheeseExtraTomato.name());
