c61d0e657e43a87695c8c37f1a6be94a507a4c66
mitredo <dimon4ikac89@gmail.com>
2026-01-19 22:57:27 +0300

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime createdItem = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = createdItem.format(formatter);
+        System.out.println(currentDateTimeFormat);
+    }
+
+}
+
