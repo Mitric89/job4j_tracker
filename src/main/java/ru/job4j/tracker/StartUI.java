8aec0f34549118d1f48bcb2c0599bfb538ea8d20
mitredo <dimon4ikac89@gmail.com>
2026-03-11 19:59:13 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,6 +10,9 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = createdItem.format(formatter);
         System.out.println(currentDateTimeFormat);
+
+        Item item1 = new Item("name");
+        System.out.println(item1);
     }
 
 }
