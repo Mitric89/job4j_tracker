0f1250973067174fa810937bf9d5ca9b25f508d4
mitredo <dimon4ikac89@gmail.com>
2026-04-06 17:59:18 +0300

    1. Singleton [#94606]

---
+++ src/test/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private static SingleTracker instance;
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+}
